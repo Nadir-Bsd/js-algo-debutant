cedd4f4fb31b283a8f35d498decb51e672950db3
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-11-12 09:33:58 +0100

    4 exo

---
+++ exo_4.js
@@ -0,0 +1,9 @@
+
+for(let i=1; i <= 20; i++){
+
+    if(i%2 == 1 ){
+        console.log("le nombre: "+i+" est inpaire\n");
+    }else{
+        console.log("le nombre: "+i+" est paire\n");
+    }
+};
